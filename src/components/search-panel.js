00d551308b8b2b674bf8a209d115e8190de90f84
Diana Rukavishnikova <psihomatika@gmail.com>
2020-12-21 15:14:59 +0300

    первый коммит

---
+++ src/components/search-panel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ItemStatusFilter from './item-status-filter';
+
+import './search-panel.css';
+
+const SearchPanel = () => {
+    const searchtext = 'search';
+    return (
+      <div className="search-panel">
+        <input className="search-input"
+              placeholder= {searchtext} />
+        <ItemStatusFilter />
+      </div>
+    
+  );
+};
+
+export default SearchPanel;
---
+++ src/components/todo-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
+// в TodoListItem заменила label = {item.label} important={item.important} 
+//при помощи спред оператора {...item } передает все свойиства нужного компонента. 
+//key, обычно получают из базы данных (id)но лучше не использовать 1 2 3 
+const TodoList = ({ todos }) => {
+
+    const elements = todos.map((item) => {
+
+        const { id, ...itemProps } = item;
+
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem { ...itemProps  }/>     
+            </li>
+        );
+    });
+    return(  
+        <ul className="list-group todo-list">
+            { elements }
+        </ul>
+    );
+};
+
+export default TodoList;
